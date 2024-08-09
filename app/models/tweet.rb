e35bf0d4a633ef2c84d4abeec3c04c0c99a8d3b6
okok111 <1for.rails@gmail.com>
2024-08-10 01:15:05 +0900

    7-10

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,8 +5,7 @@ class Tweet < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_many :comments, dependent: :destroy
-  validates :name, presence: true #追記
-  validates :profile, length: { maximum: 200 } 
+ 
   has_many :tweet_tag_relations, dependent: :destroy
   has_many :tags, through: :tweet_tag_relations, dependent: :destroy
 end
